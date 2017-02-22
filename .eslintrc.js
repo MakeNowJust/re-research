2ab9586a3f3d67af22308d277b9053c934f62656
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-02-22 11:22:45 +0900

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: `eslint:recommended`,
+  rules: {
+    [`linebreak-style`]: [`error`, `unix`],
+    curly: [`error`, `multi-or-nest`, `consistent`],
+    indent: [`error`, 2, {SwitchCase: 1}],
+    quotes: [`error`, `backtick`],
+    semi: [`error`, `never`],
+    strict: [`error`, `global`]
+  }
+}
